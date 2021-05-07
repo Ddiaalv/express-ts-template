24874954e3c0183613980f70b415f7978f176c6c
ddiaalv <ddiaalv@gmail.com>
2021-05-07 16:20:56 +0100

    :eyes: Eslint

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,26 @@
-import express, { Application, NextFunction, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from 'express'
+import path from 'path'
 
-const app: Application = express();
-const PORT = process.env.PORT || 3001;
+const app: Application = express()
+const PORT = process.env.PORT || 3001
 
-app.use("/img", express.static(__dirname + "/public"));
+app.use('/img', express.static(path.join(__dirname, '/public')))
 
 app.use((req: Request, res: Response, next: NextFunction) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Origin', '*')
   res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  next()
+})
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + Typescript Server");
-});
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + Typescript Server')
+})
 
 app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
-});
+  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`)
+})
 
-export default app;
+export default app

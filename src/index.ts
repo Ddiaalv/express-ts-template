244d98e13962afcbf0fda2d7b55ae0b2b5a27637
ddiaalv <ddiaalv@gmail.com>
2021-05-07 15:59:26 +0100

    :no_entry: Enable CORS

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 
 const app: Application = express();
 const PORT = process.env.PORT || 3001;
 
 app.use("/img", express.static(__dirname + "/public"));
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + Typescript Server");
